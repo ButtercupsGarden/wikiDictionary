3e7c2743d629f503f401733169235064b0741005
Unilfo <kokopal13@gmail.com>
2021-01-21 13:51:48 +0300

    базовый бэк показать все показать активные добавить

---
+++ wiki/util/test.js
@@ -0,0 +1,5 @@
+const {getAllPhrases, getActivePhrases, addPhrase} = require('./util')
+
+// console.log(getActivePhrases())
+// console.log(getAllPhrases())
+// console.log(addPhrase('test','тест'))
---
+++ wiki/util/util.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+const uri = '../db/data.JSON'
+let data = fs.readFileSync(uri)
+
+function getAllPhrases() {
+  return JSON.parse(data)
+}
+
+function getActivePhrases() {
+  return JSON.parse(data).filter((phrase) => phrase.active)
+}
+
+function addPhrase(phrase, translate, active = false) {
+  let phrases = JSON.parse(data)
+  let maxId = Math.max(...phrases.map(i => i.id))
+
+  let newPhrase = {
+    id: maxId + 1,
+    phrase: phrase,
+    translate: translate,
+    active: active
+  }
+
+  phrases.push(newPhrase)
+  fs.writeFileSync('../db/data.JSON', JSON.stringify(phrases, null, 2))
+  return newPhrase
+}
+
+module.exports = {
+  getAllPhrases,
+  getActivePhrases,
+  addPhrase
+}
